79dec598090f34259e1f581e3a5b21f7fb6e172a
tomatovine1 <hesmaeilz01@gmail.com>
2022-11-20 22:22:19 -0500

    single card component

---
+++ src/components/SingleCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Card = ({ symbol }) => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="card">
+            <div className="card_body">
+                <h2 className="card_title">{symbol}</h2>
+            </div>
+            <button className="card_btn" onClick={() => navigate(`/stocks/:${symbol}`, { state: symbol})}>View Details</button>
+        </div>
+    );
+}
+
+export default Card;
